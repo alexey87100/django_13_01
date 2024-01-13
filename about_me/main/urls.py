7b121fc2b365fd36592f14bc4c91a6bf22a2db1c
alex <kaschlinoff2011@yandex.ru>
2024-01-13 15:30:45 +0300

    final result

--- about_me/main/urls.py
+++ about_me/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import index
+from .views import index, detail
 
 app_name = 'main'
 
 
 urlpatterns = [
-    path('index/', index, name='index'),
+    path('', index, name='index'),
+    path('detail/<int:employee_id>/', detail, name='detail')
 ]
--- about_me/main/views.py
+++ about_me/main/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
+
+from .data import EMPLOYEES, SKILLS
 
 
 def index(request):
     """Главная страница."""
-    weather = {'temp': '-20',
-               'pres': '740'}
-    context = {'hello_text': 'Привет, пользователь!',
-               'weather': weather}
+    context = {
+        'employees': EMPLOYEES
+    }
     return render(request, 'index.html', context)
+
+
+EMPLOYEES_DICT = {employee['id']: employee for employee in EMPLOYEES}
+
+SKILLS_ID = {skill['id']: skill for skill in SKILLS}
+
+
+def detail(request, employee_id):
+
+    if employee_id in EMPLOYEES_DICT.keys():
+        employee = EMPLOYEES_DICT[employee_id].copy()
+        list_skills = []
+        for skill_id in employee['skills']:
+            for elem in SKILLS:
+                if skill_id == elem['id']:
+                    list_skills.append(elem['name'])
+                    break
+        employee['skills'] = list_skills
+        context = {
+            'employee': employee
+        }
+        return render(request, 'detail.html', context)
+    raise Http404("Нет такого сотрудника")
